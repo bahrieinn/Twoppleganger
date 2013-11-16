bc783f23fe959d90c315e9f3ba43db98992bb838
Brian Tong <btong34@gmail.com>
2013-11-16 15:23:46 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twoppleganger::Application.config.secret_key_base = 'f631f0bbdf3905140b8ba35db35f40acdecc18b8eee8ee45e0dd9d9d40d8473d6df66f45b217eb2cc32e42e2ebf81b463543f548c0ec798b1511562acd3dc171'
