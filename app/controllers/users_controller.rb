8b1a133604bdfbebbf98631a04e9be772c9592a1
Brian Tong <btong34@gmail.com>
2013-11-17 00:30:42 -0600

    start styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
       @user = User.find(session[:user_id])
       top_3_matches = @user.determine_match
       @match = top_3_matches.first
+      @match_score = @match[1]['match_score'].round
       render :match_found
     else
       redirect_to root_url, notice: "Please sign in first!"
