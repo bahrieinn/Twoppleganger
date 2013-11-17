f9e66305c7f7b71ca14c9e277ce3e9e769880ffb
Brian Tong <btong34@gmail.com>
2013-11-16 21:49:03 -0600

    add user stat columns

--- db/migrate/20131116224944_create_users.rb
+++ db/migrate/20131116224944_create_users.rb
@@ -7,6 +7,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :oauth_token
       t.string :oauth_secret
       t.string :profile_pic_url
+      t.integer :follower_count
+      t.integer :following_count
+      t.integer :tweet_count
 
       t.timestamps
     end
