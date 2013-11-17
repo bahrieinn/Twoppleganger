0953f91e6f3ffe6cb33fe13a20c6dadddb784205
Brian Tong <btong34@gmail.com>
2013-11-17 12:29:58 -0600

    fix syntax error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,13 @@ class UsersController < ApplicationController
     if current_user
       @user ||= User.find(session[:user_id])
       @top_3_matches ||= @user.determine_match
-      if @top_3_matches.empty
+      if @top_3_matches.empty?
         render :no_match_found
       else
         @match ||= @top_3_matches.first
         @match_score = @match[1]['match_score'].round
         render :match_found
-      end
-      
+      end 
     else
       redirect_to root_url, notice: "Please sign in first!"
     end
