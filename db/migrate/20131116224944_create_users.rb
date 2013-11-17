603d6c9593cb5ca96f2298a1d6d1440279db3bbc
Brian Tong <btong34@gmail.com>
2013-11-16 20:24:26 -0600

    add profile pic url to user model

--- db/migrate/20131116224944_create_users.rb
+++ db/migrate/20131116224944_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :provier
+      t.string :provider
       t.string :uid
       t.string :name
       t.string :oauth_token
       t.string :oauth_secret
+      t.string   :profile_pic_url
 
       t.timestamps
     end
