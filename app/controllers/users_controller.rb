c96d9453209efd411e28ca0bccdb45cb523df790
Brian Tong <btong34@gmail.com>
2013-11-17 19:31:26 -0600

    change logic to filter low matches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   def twop_lookup
     if current_user
       @user ||= User.find(session[:user_id])
-      @top_3_matches ||= @user.determine_match
-      if @top_3_matches.empty?
+      @top_5_matches ||= @user.determine_match
+      if @top_5_matches.none? { |match, info| info['match_score'] > 50 }
         render :no_match_found
       else
-        @match ||= @top_3_matches.first
+        @match ||= @top_5_matches.first
         @match_score = @match[1]['match_score'].round
         render :match_found
       end 
